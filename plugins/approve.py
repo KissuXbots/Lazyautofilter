10ea41f468dcc30659e06ea82cb33a0ed3253891
𓆩•𝐊𝐢𝐬𝐬𝐮 💞•𓆪 <kissuXbots@gmail.com>
2024-06-26 19:04:41 +0530

    Add files via upload

---
+++ plugins/approve.py
@@ -0,0 +1,26 @@
+import os
+import asyncio
+from pyrogram import Client, filters
+from pyrogram.types import Message, User, ChatJoinRequest
+from info import CHAT_ID, APPROVED 
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+@Client.on_chat_join_request((filters.group | filters.channel) & filters.chat(CHAT_ID) if CHAT_ID else (filters.group | filters.channel))
+async def autoapprove(client, message: ChatJoinRequest):
+    chat=message.chat 
+    user=message.from_user 
+    print(f"{user.first_name} Joined") 
+    await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
+    if APPROVED == "on":
+        buttons = [[ 
+            InlineKeyboardButton('🌟 Jᴏɪɴ Oᴜʀ Cʜᴀɴɴᴇʟ 💞', url=f'https://t.me/kissuxbots')
+            
+        ]]
+        markup = InlineKeyboardMarkup(buttons)
+        caption = f"<b>{message.from_user.mention()},\n\nʏᴏᴜʀ ʀᴇǫᴜᴇsᴛ ᴛᴏ ᴊᴏɪɴ {message.chat.title} ɪs ᴀᴘᴘʀᴏᴠᴇᴅ.</b>"
+        await client.send_photo(
+            message.from_user.id, 
+            photo='https://telegra.ph/file/49c5b13a7763651abcbac.jpg', 
+            caption=caption, 
+            reply_markup=markup
+        )
